13ea2bd3a70096d5063e06b263919a007b0c2555
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-06-11 14:00:05 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
-  attr_accessor :author, :page_count, :genre
-  attr_reader :title
+  attr_accessor :author, :page_count
+  attr_reader :title, :genre
   
   GENRES = []
 
@@ -14,6 +14,7 @@ class Book
   
   def genre=(genre)
     @genre = genre
+    GENRES << genre
   end 
 
 end
